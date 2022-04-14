ea33a5bb1dd8a6641a884b34923ed9fb4c2c5006
Napat <napat.wongchr@gmail.com>
2022-04-14 17:54:13 +0700

    add register api

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,5 +1,22 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
 
 const authRouter = Router();
+
+// โค้ดนี้อยู่ในไฟล์ server/apps/auth.js
+
+authRouter.post("/register", async (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  const collection = db.collection("users");
+  await collection.insertOne(user);
+  return res.json({
+    message: "User has been created successfully",
+  });
+});
 
 export default authRouter;

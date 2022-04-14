fbcf2920dd7a0de4bbad67cb0ca78553c5b14559
Napat <napat.wongchr@gmail.com>
2022-04-14 17:54:14 +0700

    add login on frontend

--- client/src/contexts/authentication.js
+++ client/src/contexts/authentication.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const AuthContext = React.createContext();
 
@@ -14,7 +15,15 @@ function AuthProvider(props) {
   const navigate = useNavigate();
 
   // make a login request
-  const login = () => {};
+  const login = async (data) => {
+    const result = await axios.post("http://localhost:4000/auth/login", data);
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    navigate("/");
+    const userDataFromToken = jwtDecode(token);
+    setState({ ...state, user: userDataFromToken });
+    navigate("/");
+  };
 
   // register the user
   const register = async (data) => {
